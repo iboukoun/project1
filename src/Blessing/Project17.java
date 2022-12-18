5950d587e93883cd717c8a809436dbbee6188c4b
IBOUKOUN <blessinglovejob@gmail.com>
2022-12-17 18:05:00 -0600

    Array

---
+++ src/Blessing/Project17.java
@@ -0,0 +1,22 @@
+package Blessing;
+
+public class Project17 {
+    public static void main(String[] args) {
+        int[] array1 = {4, 20, 15, 2, 19};
+
+        String[] world = {"hello", "there", "how", "are", "you?"};
+        System.out.println(array1[2]);
+        System.out.println(world[1]);
+        System.out.println(array1.length);
+        System.out.println(world.length);
+for (int i = 0;i <array1.length;i++)
+{
+ System.out.println(array1[i]);
+}
+for (int y = 0;y < world.length; y++)
+{
+    System.out.println(world[y]);
+}
+}
+
+}
