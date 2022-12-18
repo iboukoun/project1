71bf8e71577a9513473c5aedcb26941438e7de8e
IBOUKOUN <blessinglovejob@gmail.com>
2022-12-17 18:11:17 -0600

    hierarchical inheritance

---
+++ src/Blessing/Animal.java
@@ -0,0 +1,16 @@
+package Blessing;
+
+public class Animal {
+    int age;
+    String name;
+
+    public void setNameAge(String name, int age)
+    {
+        this.name=name;
+        this.age=age;
+    }
+    public void shout()
+    {
+        System.out.println("shout");
+    }
+}
